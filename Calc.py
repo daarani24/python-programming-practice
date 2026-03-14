fce517498c1960b7b0a2507daf37d90798cb9a3d
daarani24 <daarani2007@gmail.com>
2026-03-14 22:43:33 +0530

    Practiced simple calculation program

---
+++ Calc.py
@@ -0,0 +1,14 @@
+def add(a,b):
+    return a+b
+def sub(a,b):
+    return a-b
+def mul(a,b):
+    return a*b
+def div(a,b):
+    return a//b
+x=int(input("Enter 1st number:"))
+y=int(input("Enter 2nd number:"))
+print("Addition:",add(x,y))
+print("Subtraction:",sub(x,y))
+print("Multiplication:",mul(x,y))
+print("Division:",div(x,y))
